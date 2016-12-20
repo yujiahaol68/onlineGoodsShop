80003f5058bf56e3b5a16a059b70cfb189ec6e92
Tecker.Yu <767954505@qq.com>
2016-12-20 14:54:56 +0800

    import unslider jquery plugin
    New feature:
    text-slider to show announcement
    
    README update preview image

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # onlineGoodsShop
 This is a onlineGoodsShop site using foundation-cli and laravel5.3 which is front-back separated
+
+
+#### index page preview
+![](http://storage1.imgchr.com/99iGt.png)
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,11 @@
 $(document).foundation();
 $('#logoImg').addClass('animated zoomInLeft');
+$('.Announcement').unslider({
+	autoplay: true,
+	infinite: true,
+	arrows: false,
+	nav: false
+});
 $('img').unveil();
 $('body').materialScrollTop({
 	duration: 600
